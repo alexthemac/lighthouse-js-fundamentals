a4ad7aa212a00b3f2c9b69fea4786a1e4564869c
Alex MacLure <almaclure@gmail.com>
2021-05-31 02:08:51 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,12 @@
 const temperature = 12;
 
 if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
+  	console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
+  	console.log("Short sleeves won't cut it!");
+} else if (temperature < -40 || temperature > 40) {
+	console.log("maybe going outside isn't such a great idea...");
 } else {
-  console.log("Short sleeves are fine.");
+  	console.log("Short sleeves are fine.");
 }
-
 console.log("Now you're ready to go outside!");
